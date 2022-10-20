419c1fa27a49620aa3d8f9de9bff9cb9bf53ec24
Bektur1902 <bektur1902@gmail.com>
2022-10-20 12:37:39 +0600

    Change settings, Add models and makemigrations

--- courses/settings.py
+++ courses/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'main',
 ]
 
 MIDDLEWARE = [
@@ -74,9 +76,15 @@ WSGI_APPLICATION = "courses.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'courses_app',
+        'USER': 'bektur',
+        'PASSWORD': '9569',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
--- main/models.py
+++ main/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+CONTACT_CHOISES = (
+    (1, 'PHONE'),
+    (2, 'Facebook'),
+    (3, 'Email')
+)
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    discription = models.CharField(max_length=100)
+    logo = models.CharField(max_length=100, blank=True, null=True)
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    imgpath = models.CharField(max_length=100)
+
+class Branch(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    latitude = models.CharField(max_length=100)
+    longtude = models.CharField(max_length=100)
+    adress = models.CharField(max_length=100)
+
+class Conctact(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    type = models.IntegerField(choices=CONTACT_CHOISES, blank=True, null=True)
+    value = models.CharField(max_length=100)
